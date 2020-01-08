b94feafcdc1816627a4570ec9f1683e06cb011aa
sophie <s.hautcoeur@gmail.com>
2020-01-08 14:46:27 -0500

    added redux

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
+import { connect } from 'react-redux';
 
 
 const Header = ({ currentUser }) => (
@@ -27,4 +28,8 @@ const Header = ({ currentUser }) => (
   </div>
 );
 
-export default Header;
+const mapStatToProps = state => ({
+  currentUser: state.user.currentUser
+})
+
+export default connect(mapStatToProps)(Header);
