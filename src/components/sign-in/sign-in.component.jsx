66ca7e55b05e4ff34fd1959969cfc437c9063a8c
sophie <s.hautcoeur@gmail.com>
2020-03-22 13:39:55 +0200

    refactoring with sagas

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -2,7 +2,9 @@ import React from 'react';
 import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
-import { auth, signInWithGoogle } from '../../firebase/firebase.utils';
+import { googleSignInStart, emailSignInStart } from '../../redux/user/user.actions';
+import { connect } from 'react-redux';
+
 
 class SignIn extends React.Component{
   constructor(props){
@@ -16,17 +18,11 @@ class SignIn extends React.Component{
 
   handleSubmit = async event => {
     event.preventDefault();
-
+    const {emailSignInStart} = this.props;
     const {email, password} = this.state;
 
-    try {
-      await auth.signInWithEmailAndPassword(email, password);
-      this.setState({ email: '', password: '' });
-    } catch (error) {
-      console.log(error);
-    }
-
-  }
+    emailSignInStart(email, password);
+  };
 
   handleChange = event => {
     const { value, name } = event.target;
@@ -35,6 +31,7 @@ class SignIn extends React.Component{
   }
 
   render() {
+    const { googleSignInStart } = this.props;
     return(
       <div className='sign-in'>
         <h2>I already have an account</h2>
@@ -58,7 +55,7 @@ class SignIn extends React.Component{
           />
           <div className='buttons'>
           <CustomButton type="submit">Sign in</CustomButton>
-          <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+          <CustomButton type='button' onClick={googleSignInStart} isGoogleSignIn>
             {' '}
             Sign in with Google{' '}
           </CustomButton>
@@ -69,4 +66,9 @@ class SignIn extends React.Component{
   }
 }
 
-export default SignIn;
+const mapDispatchToProps = dispatch => ({
+  googleSignInStart: () => dispatch(googleSignInStart()),
+  emailSignInStart: (email, password) => dispatch(emailSignInStart({email, password}))
+});
+
+export default connect(null, mapDispatchToProps)(SignIn);
--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -18,6 +18,11 @@ const cartReducer = (state = INITIAL_STATE, action) => {
         ...state,
         cartItems: addItemToCart(state.cartItems, action.payload)
       };
+    case CartActionTypes.REMOVE_ITEM:
+      return {
+        ...state,
+        cartItems: removeItemFromCart(state.cartItems, action.payload)
+      };
     case CartActionTypes.CLEAR_ITEM_FROM_CART:
       return {
         ...state,
@@ -25,13 +30,13 @@ const cartReducer = (state = INITIAL_STATE, action) => {
           cartItem => cartItem.id !== action.payload.id
         )
       };
-    default:
-      return state;
-    case CartActionTypes.REMOVE_ITEM:
+    case CartActionTypes.CLEAR_CART:
       return {
         ...state,
-        cartItems: removeItemFromCart(state.cartItems, action.payload)
-      };
+        cartItems: []
+      }
+    default:
+      return state;
   }
 }
 
