949971d28afc39c15464cb0d7454fa99d83e8b20
sophie <s.hautcoeur@gmail.com>
2020-03-28 20:36:43 +0300

    fixing firebase utils

--- client/src/firebase/firebase.utils.js
+++ client/src/firebase/firebase.utils.js
@@ -2,14 +2,14 @@ import firebase from 'firebase/app';
 import 'firebase/firestore';
 import 'firebase/auth';
 
-const config = {
-    apiKey: "AIzaSyDNLMD3FHpPXrPvsY2AFY7P6g8eUQYrWf8",
-    authDomain: "crwn-db-70945.firebaseapp.com",
-    databaseURL: "https://crwn-db-70945.firebaseio.com",
-    projectId: "crwn-db-70945",
-    storageBucket: "crwn-db-70945.appspot.com",
-    messagingSenderId: "1006819212853",
-    appId: "1:1006819212853:web:1d6fe6dbc7f7bbfc41bb7c"
+const firebaseConfig = {
+  apiKey: "AIzaSyDNLMD3FHpPXrPvsY2AFY7P6g8eUQYrWf8",
+  authDomain: "crwn-db-70945.firebaseapp.com",
+  databaseURL: "https://crwn-db-70945.firebaseio.com",
+  projectId: "crwn-db-70945",
+  storageBucket: "crwn-db-70945.appspot.com",
+  messagingSenderId: "1006819212853",
+  appId: "1:1006819212853:web:1d6fe6dbc7f7bbfc41bb7c"
 };
 
 firebase.initializeApp(config);
